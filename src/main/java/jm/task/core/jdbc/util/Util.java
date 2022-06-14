6f9ea8ea6665c99d99531869785590b51986630c
Boldinov <pashaboldinov@gmail.com>
2022-06-14 21:00:18 +0500

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Pasha19980906";
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        return connection;
+    }
+
+
+
 }
