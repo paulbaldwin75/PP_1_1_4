bbd63582248ca8f7d10e13aed9c709d11ce298ee
Boldinov <pashaboldinov@gmail.com>
2022-06-20 18:00:34 +0500

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         userService.createUsersTable();
 
-        userService.saveUser("Name1", "LastName1", (byte) 20);
+        userService.saveUser("Ivan", "LastName1", (byte) 20);
         userService.saveUser("Name2", "LastName2", (byte) 25);
         userService.saveUser("Name3", "LastName3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
